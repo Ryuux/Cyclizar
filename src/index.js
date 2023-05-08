3a92e759fe1080bcbd1388bae747d23fc19db007
Ryuux <iloveryuux@gmail.com>
2023-05-08 02:06:05 -0400

    feat: connected database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const { Client, Collection } = require('discord.js');
 const dotenv = require('dotenv');
 const fs = require('fs');
+const connect = require('./database/connect');
 
 const { CLIENT_OPTIONS, CHANNEL_ID, MESSAGE_WELCOME } = require('./config');
 
 const client = new Client(CLIENT_OPTIONS);
 dotenv.config();
+connect(process.env.MONGODB_URI)
 
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.cache.get(CHANNEL_ID);
